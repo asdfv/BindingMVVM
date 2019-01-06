70f06f423a95e0df78f1e73153960010d6925e21
asdfv <grodno.by@mail.ru>
2019-01-06 18:10:38 +0300

    Fix @Bindable

--- app/src/main/java/by/grodno/vasili/bindingmvvm/Phone.java
+++ app/src/main/java/by/grodno/vasili/bindingmvvm/Phone.java
@@ -1,7 +1,5 @@
 package by.grodno.vasili.bindingmvvm;
 
-import android.databinding.Bindable;
-
 public class Phone {
     private String brand;
     private int year;
@@ -13,30 +11,15 @@ public class Phone {
         this.model = model;
     }
 
-    @Bindable
     public String getBrand() {
         return brand;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    @Bindable
     public int getYear() {
         return year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    @Bindable
     public String getModel() {
         return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
     }
 }
